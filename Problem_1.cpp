1db2c7c45350edd86ff53477620f4a42acde80cc
yaaaminiii <yaminibudala29@gmail.com>
2025-07-19 11:11:34 +0530

    Add files via upload

---
+++ Problem_1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+	class student{
+		private:
+			int roll_no;
+			int marks;
+	    public:
+	    	string name;
+	    	string branch;
+	    student(int r,int m,string n,string b)
+	    {
+	    	roll_no=r;
+	    	marks=m;
+	    	name=n;
+	    	branch=b;	
+		}
+		void setmarks(int m)
+		{
+			marks=m;}
+		int getmarks()
+		{
+			return marks;}
+		void displaydetails()
+		{cout << "Student Name: " << name << endl;
+        cout << "Branch: " << branch << endl;
+        cout << "Roll No: " << roll_no << endl;
+        cout << "Marks: " << marks << endl;
+		}
+	};
+	int main()
+	{
+		student s1(1,99,"yamini","cse");
+		s1.displaydetails();
+		s1.setmarks(98);
+		cout<<"updated_marks"<<s1.getmarks()<<endl;
+	}
+
