ab56586334980b62d02c87e0dc68c9d13913a367
yaaaminiii <yaminibudala29@gmail.com>
2025-07-24 20:44:22 +0530

    Update copy_constructor.cpp

--- copy_constructor.cpp
+++ copy_constructor.cpp
@@ -9,6 +9,12 @@ class student{
 		name=s;
 		cgpa=c;
 	} 
+//copying a constructor
+        student(student &s)
+	{
+		name=s.name;
+		cgpa=s.cgpa;
+	}
 	void getinfo() 
 	{
 		cout<<name<<" "<<cgpa<<endl;
@@ -18,6 +24,6 @@ int main()
 {
 	student s("yamini",9.0);
 	s.getinfo();
-	student s1(s);
+	student s1(s);//by default it automatically copies the constructor 
 	s1.getinfo();
 }
